6841a05a4a4ab2bc9abe6047fc4c98e3999b31cf
Michael Li <github@ngli.me>
2023-05-23 15:19:48 -0400

    Added longest substring

---
+++ longest_substring_without_repeating_characters/longest.py
@@ -0,0 +1,38 @@
+class Solution:
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        chars = {}
+        left = right = 0
+        longest = 0
+
+        while right < len(s):
+            r = s[right]
+            if r in chars:
+                chars[r] += 1
+            else:
+                chars[r] = 1
+
+            while chars[r] > 1:
+                l = s[left]
+
+                if l in chars:
+                    chars[l] -= 1
+                else:
+                    chars[l] = -1
+
+                left +=1
+
+            longest = max(longest, right - left + 1)
+            right += 1
+            print(left, right, chars)
+        return longest
+
+def main():
+    s = Solution()
+    input = "abcabcbb"
+    print(s.lengthOfLongestSubstring(input));
+
+
+if __name__ == "__main__":
+    main()
+
